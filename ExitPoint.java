ed7eec86ed061bff07ce17c2c73c086cb95e39a4
branthebuilder05 <30659231+branthebuilder05@users.noreply.github.com>
2023-11-01 20:31:34 +0000

    spiral

---
+++ ExitPoint.java
@@ -0,0 +1,5 @@
+public class ExitPoint {
+    public static void main(String[] args) {
+        
+    }
+}
--- waveTraversal.java
+++ waveTraversal.java
@@ -1,3 +1,41 @@
 public class waveTraversal {
-    
+    public static void main(String[] args) {
+        int arr[][] = { { 1, 2, 3, 4 }, { 5, 6, 7, 8 }, { 9, 10, 11, 12 }, { 13, 14, 15, 16 } };
+        int minrow, mincol, maxrow, maxcol;
+        minrow = 0;
+        mincol = 0;
+        maxrow = arr.length - 1;
+        maxcol = arr[0].length - 1;
+
+        while (mincol<=maxcol || minrow<=mincol) {
+            //// mincolfixed
+            for (int i = minrow, j = mincol; i <= maxrow; i++) {
+                System.out.println(arr[i][j] + "from 1st");
+            }
+            mincol++;
+            // maxrow fixed
+            for (int i = maxrow, j = mincol; j <= maxcol; j++) {
+                System.out.println(arr[i][j]+ "from 2nd");
+            }
+            maxrow--;
+
+            // maxcol fixed
+            for (int i = maxrow, j = maxcol; i >= minrow; i--) {
+                System.out.println(arr[i][j] + "from 3rd");
+            }
+            maxcol--;
+
+            // minrow fixed
+            for (int i = minrow, j = maxcol; j >= mincol; j--) {
+                System.out.println(arr[i][j] + "from 4th");
+            }
+            minrow++;
+
+        }
+
+
+            
+        
+
+    }
 }
