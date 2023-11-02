b5cd60b4b166174672581ce46ce19f1e73af7d76
branthebuilder05 <30659231+branthebuilder05@users.noreply.github.com>
2023-11-02 22:22:25 +0530

    Update diagonal.java

--- diagonal.java
+++ diagonal.java
@@ -15,3 +15,27 @@ public class Main
 		
 	}
 }
+
+
+
+
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		int arr[][]={{1,2,3,4},{5,6,7,8},{1,2,3,4},{5,6,7,8}};
+		
+// 		for(int i=arr.length-1,k=0;i>=0;i--,k++){
+// 		    for(int j=0,l=k;j<=i;j++,l++){
+// 		        System.out.println(arr[j][l]+" ");
+// 		    }
+// 		}
+
+        for(int i=0;i<arr.length;i++)
+            for (int j=0;j<arr.length-i;j++)
+                     System.out.println(arr[j][j+i]+" ");
+		
+		
+		
+	}
+}
