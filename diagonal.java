d85aaa95d164e38077564db1fdc39ff1ff55a712
branthebuilder05 <30659231+branthebuilder05@users.noreply.github.com>
2023-11-02 22:14:27 +0530

    Create diagonal.java

---
+++ diagonal.java
@@ -0,0 +1,17 @@
+
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		int arr[][]={{1,2,3,4},{5,6,7,8},{1,2,3,4},{5,6,7,8}};
+		
+		for(int i=arr.length-1,k=0;i>=0;i--,k++){
+		    for(int j=0,l=k;j<=i;j++,l++){
+		        System.out.println(arr[j][l]+" ");
+		    }
+		}
+		
+		
+		
+	}
+}
